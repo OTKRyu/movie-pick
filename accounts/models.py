ab125f7bdf1c98948749820e246bb29b53088848
OTKRyu <appn12@gmail.com>
2021-05-24 16:02:07 +0900

    ratefield changed: interget => float

--- accounts/models.py
+++ accounts/models.py
@@ -10,16 +10,15 @@ class User(AbstractUser):
     movie_to_see = models.ManyToManyField(Movie, 'users_will_see', through='See')
     rated_movies = models.ManyToManyField(Movie, related_name='user_rates',through='Rate')
     
-
     def __str__(self):
-        return self.username
+        return self.nickname
 
 
 class Rate(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_to_rate')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_to_rate')
     series = models.ForeignKey(Series, on_delete=models.CASCADE)
-    rate = models.IntegerField()
+    rate = models.FloatField()
     comment = models.CharField(max_length=50)
 
 class See(models.Model):
